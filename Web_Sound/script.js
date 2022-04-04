1579ec97f5cd3763159edcde8b7e7599d9792381
Rhea-Braithwaite <rfb7689@nyu.edu>
2022-04-04 20:43:24 +0400

    Fixed Gauge and added era pcitures

--- Web_Sound/script.js
+++ Web_Sound/script.js
@@ -3,3 +3,18 @@ AOS.init();
 $('#arrow').click(function(){
 $(document).scrollTop(850) // any value you need
 });
+
+let fill = document.getElementById("gauge-fill");
+function scroll (){ //Make additional content of webapge visible after the user begins scrolling
+    var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+    var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+    var scrolled = (winScroll / height) * 100 ;
+    var max = 90;
+    console.log(check);
+    fill.style.height = max - scrolled + "%";
+
+}
+
+window.onscroll = function() {
+  scroll();
+}
