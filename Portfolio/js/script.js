aa9201b302acb4c00bba9edff4354587c4aebc1a
Rhea-Braithwaite <rfb7689@nyu.edu>
2022-05-09 23:50:26 +0400

    Flipbook

---
+++ Portfolio/js/script.js
@@ -0,0 +1,188 @@
+AOS.init();
+
+//On click if audio is not playing
+//playing
+//On click if audio is playing then
+//stop playing
+
+var Page1 = (function() {
+
+  var config = {
+      $bookBlock1 : $( '#bb-bookblock1' ),
+      $navNext1 : $( '#bb-nav-next1' ),
+      $navPrev1 : $( '#bb-nav-prev1' ),
+    },
+    init = function() {
+      config.$bookBlock1.bookblock( {
+        speed : 800,
+        shadowSides : 0.8,
+        shadowFlip : 0.7
+      } );
+      initEvents();
+    },
+    initEvents = function() {
+
+      var $slides = config.$bookBlock1.children();
+
+      // add navigation events
+      config.$navNext1.on( 'click touchstart', function() {
+        config.$bookBlock1.bookblock( 'next' );
+        return false;
+      } );
+
+      config.$navPrev1.on( 'click touchstart', function() {
+        config.$bookBlock1.bookblock( 'prev' );
+        return false;
+      } );
+
+
+      // add swipe events
+      // $slides.on( {
+      //   'swipeleft' : function( event ) {
+      //     config.$bookBlock.bookblock( 'next' );
+      //     return false;
+      //   },
+      //   'swiperight' : function( event ) {
+      //     config.$bookBlock.bookblock( 'prev' );
+      //     return false;
+      //   }
+      // } );
+      //
+      // // add keyboard events
+      // $( document ).keydown( function(e) {
+      //   var keyCode = e.keyCode || e.which,
+      //     arrow = {
+      //       left : 37,
+      //       up : 38,
+      //       right : 39,
+      //       down : 40
+      //     };
+      //
+      //   switch (keyCode) {
+      //     case arrow.left:
+      //       config.$bookBlock.bookblock( 'prev' );
+      //       break;
+      //     case arrow.right:
+      //       config.$bookBlock.bookblock( 'next' );
+      //       break;
+      //   }
+      // } );
+    };
+
+    return { init : init };
+
+})();
+
+Page1.init();
+
+var Page2 = (function() {
+
+  var config = {
+      $bookBlock2 : $( '#bb-bookblock2' ),
+      $navNext2 : $( '#bb-nav-next2' ),
+      $navPrev2 : $( '#bb-nav-prev2' ),
+    },
+    init = function() {
+      config.$bookBlock2.bookblock( {
+        speed : 800,
+        shadowSides : 0.8,
+        shadowFlip : 0.7
+      } );
+      initEvents();
+    },
+    initEvents = function() {
+
+      var $slides = config.$bookBlock2.children();
+
+      // add navigation events
+      config.$navNext2.on( 'click touchstart', function() {
+        config.$bookBlock2.bookblock( 'next' );
+        return false;
+      } );
+
+      config.$navPrev2.on( 'click touchstart', function() {
+        config.$bookBlock2.bookblock( 'prev' );
+        return false;
+      } );
+    };
+
+    return { init : init };
+
+})();
+
+Page2.init()
+
+
+var Page3 = (function() {
+
+  var config = {
+      $bookBlock3 : $( '#bb-bookblock3' ),
+      $navNext3 : $( '#bb-nav-next3' ),
+      $navPrev3 : $( '#bb-nav-prev3' ),
+    },
+    init = function() {
+      config.$bookBlock3.bookblock( {
+        speed : 800,
+        shadowSides : 0.8,
+        shadowFlip : 0.7
+      } );
+      initEvents();
+    },
+    initEvents = function() {
+
+      var $slides = config.$bookBlock3.children();
+
+      // add navigation events
+      config.$navNext3.on( 'click touchstart', function() {
+        config.$bookBlock3.bookblock( 'next' );
+        return false;
+      } );
+
+      config.$navPrev3.on( 'click touchstart', function() {
+        config.$bookBlock3.bookblock( 'prev' );
+        return false;
+      } );
+    };
+
+    return { init : init };
+
+})();
+
+Page3.init()
+
+var Page4 = (function() {
+
+  var config = {
+      $bookBlock4 : $( '#bb-bookblock4' ),
+      $navNext4 : $( '#bb-nav-next4' ),
+      $navPrev4 : $( '#bb-nav-prev4' ),
+    },
+    init = function() {
+      config.$bookBlock4.bookblock( {
+        speed : 800,
+        shadowSides : 0.8,
+        shadowFlip : 0.7
+      } );
+      initEvents();
+    },
+    initEvents = function() {
+
+      var $slides = config.$bookBlock4.children();
+
+      // add navigation events
+      config.$navNext4.on( 'click touchstart', function() {
+        config.$bookBlock4.bookblock( 'next' );
+        return false;
+      } );
+
+      config.$navPrev4.on( 'click touchstart', function() {
+        config.$bookBlock4.bookblock( 'prev' );
+        return false;
+      } );
+    };
+
+    return { init : init };
+
+})();
+
+Page4.init()
--- Portfolio/script.js
+++
@@ -1,6 +0,0 @@
-AOS.init();
-
-//On click if audio is not playing
-//playing
-//On click if audio is playing then
-//stop playing 
